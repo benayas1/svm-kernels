f116a64f67f9667c695a0b2d2d3ba2c855a622ed
Benayas <benayas1@gmail.com>
2019-11-05 23:39:13 +0100

    refactoring
    refactoring ang G function

--- kernels.py
+++ kernels.py
@@ -1,50 +1,17 @@
 import numpy as np
-
-def hermite_poly(x, degree):
-    if degree == 0:
-        return 1
-    if degree == 1:
-        return x
-    if degree > 1:
-        return np.dot(x.T, hermite_poly(x, degree - 1)) - degree * hermite_poly(x, degree - 2)
+from k_functions import k_chebyshev, k_hermite
 
 
-def k_hermite(x, z, degree):
-    k = 0
-    for i in range(0, degree):
-        k = k + np.dot(hermite_poly(x, degree), hermite_poly(z, degree))
-    return k / (np.sqrt(np.pi) * np.math.factorial(degree) / (2 ** degree))
 
-
-def hermite(degree=2):
-    k = k_hermite
-
+def g(k, **kwargs):
     def kernel(X, Z):
-        return np.array([np.array([k(x, z, degree) for z in Z]) for x in X])
+        return np.array([np.array([k(x, z, kwargs) for z in Z]) for x in X])
 
     return kernel
 
-
-def chebyshev_poly(x, degree):
-    if degree == 0:
-        return 1
-    if degree == 1:
-        return x
-    if degree > 1:
-        return np.dot(2 * x.T, chebyshev_poly(x, degree - 1)) - chebyshev_poly(x, degree - 2)
-
-
-def k_chebyshev(x, z, degree):
-    k = 0
-    for i in range(0, degree):
-        k = k + np.dot(chebyshev_poly(x, degree), chebyshev_poly(z, degree))
-    return k / (np.sqrt(len(x) - np.dot(x, z)))
+def hermite(degree=2):
+    return g(k_hermite, degree=degree)
 
 
 def chebyshev(degree=2):
-    k = k_chebyshev
-
-    def kernel(X, Z):
-        return np.array([np.array([k(x, z, degree) for z in Z]) for x in X])
-
-    return kernel
+    return g(k_chebyshev, degree=degree)
